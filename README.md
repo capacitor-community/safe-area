36e234e6789c38ff2da9608cf6a81d14d37e9b15
tafelnl <35837839+tafelnl@users.noreply.github.com>
2025-11-10 15:48:36 +0100

    feat: add polyfill logic

--- README.md
+++ README.md
@@ -1,14 +1,103 @@
-## Safe Area Experiment
+<p align="center"><br><img src="https://user-images.githubusercontent.com/236501/85893648-1c92e880-b7a8-11ea-926d-95355b8175c7.png" width="128" height="128" /></p>
+<h3 align="center">Safe Area</h3>
+<p align="center"><strong><code>@capacitor-community/safe-area</code></strong></p>
+<p align="center">
+  Capacitor Plugin that patches the safe area for older versions of Chromium
+</p>
 
-> [!IMPORTANT]
-> The Chromium team [promised](https://issues.chromium.org/issues/40699457#comment64) the bug has been fixed as of Chromium M140.
+<p align="center">
+  <img src="https://img.shields.io/maintenance/yes/2025?style=flat-square" />
+  <a href="https://www.npmjs.com/package/@capacitor-community/safe-area"><img src="https://img.shields.io/npm/l/@capacitor-community/safe-area?style=flat-square" /></a>
+<br>
+  <a href="https://www.npmjs.com/package/@capacitor-community/safe-area"><img src="https://img.shields.io/npm/dw/@capacitor-community/safe-area?style=flat-square" /></a>
+  <a href="https://www.npmjs.com/package/@capacitor-community/safe-area"><img src="https://img.shields.io/npm/v/@capacitor-community/safe-area?style=flat-square" /></a>
+</p>
+
+## Introduction
+
+On web and iOS the safe area insets work perfectly fine out of the box<sup>1</sup>. That is, on these platforms the `env(safe-area-inset-*)` CSS variables will have the correct values by default. On Android (in combination with Capacitor), however, those CSS variables will not always have the correct values when Edge-to-Edge mode is enabled. So we need to work some magic in order to have correct behavior. This plugin does that by detecting the Chromium version a user has installed. If a user has a Chromium version lower than 140, this plugin makes sure the webview gets the safe area as a margin. The `env(safe-area-inset-*)` values will be set to `0px`. That means for the Chromium webview versions with a bug, the developer doesn't have to worry about safe area insets at all. For all other versions, the developer should handle the safe area insets just as he would on web or iOS (by using the `env(safe-area-inset-*)` CSS variables). In short you can think of this plugin as a polyfill for older webview versions.
+
+> [!NOTE]
 >
-> Now it's up to the Capacitor team to correctly implement this and provide some documentation around it.
+> <sup>1</sup> As with all web applications, you still need to enable the use the variables, by telling the browser to
+> use the whole available space on the screen by adding a new viewport meta value:
 >
-> See the following issue for more info: https://github.com/ionic-team/capacitor/issues/8121
+> ```html
+> <meta name="viewport" content="viewport-fit=cover" />
+> ```
+>
+> More info on the [Mozilla website](https://developer.mozilla.org/en-US/docs/Web/CSS/env#usage)
 
-### History
+## Installation
 
-This plugin tried to workaround a Chromium bug that caused safe area inset variables to be incorrect. Chromium incorrectly provided the CSS env variables `env(safe-area-inset-*)` with a `0px` value. This plugin worked around that by retrieving the correct values in Kotlin and exposing them in CSS directly by using custom `var(--safe-area-inset-*)` variables. This worked alright. However, there were a few edge cases that were hard to catch and workaround correctly.
+```bash
+npm install @capacitor-community/safe-area@beta
+npx cap sync
+```
 
-If you want to check out the old code and documentation, see the [v7 branch](https://github.com/capacitor-community/safe-area/tree/v7)
+## Usage
+
+The plugin itself is enabled automatically. Additionally, you should enable native edge-to-edge mode in your `MainActivity` like so:
+
+Java:
+
+```java
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this); // enable edge-to-edge mode
+    }
+}
+```
+
+Kotlin:
+
+```kotlin
+class MainActivity : BridgeActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge() // enable edge-to-edge mode
+    }
+}
+```
+
+Additionally this plugin provides a helper method to style the system bars and its content. Currently, this only works by calling it from the `MainActivity`. For example:
+
+```kotlin
+SafeAreaPlugin.setSystemBarsStyle(this, SafeAreaPlugin.SystemBarsStyle.DARK)
+```
+
+## Quirks
+
+Currently there are a few known quirks:
+
+### `@capacitor/keyboard`
+
+The keyboard plugin by Capacitor has a configuration prop called `resizeOnFullScreen`. You should prevent to utilize that in your code by either omitting it or setting it to `false`. Otherwise it would interfere with the logic in this plugin. The bug that the Capacitor team is trying to workaround with that prop is already accounted for in this plugin. So you should be good to go a don't worry about it at all.
+
+### `@capacitor/status-bar`
+
+This plugin is not (yet) tested for use in combination with the status bar plugin. Ideally you should refrain from using it. Just using the `StatusBar.setStyle` should be safe though.
+
+### `adjustMarginsForEdgeToEdge` setting
+
+Capacitor provides a setting called `adjustMarginsForEdgeToEdge` which does a similar thing this plugin does when it detects a broken webview. It's advised to set this value to `disable` to prevent interference. On a similar note, you should probably remove `windowOptOutEdgeToEdgeEnforcement` if you've set that in the past.
+
+### Other safe area plugins
+
+If you've installed any other safe area plugin, you should remove them to prevent interference.
+
+These can include `@capawesome/capacitor-android-edge-to-edge-support`, `capacitor-plugin-safe-area`, `@aashu-dubey/capacitor-statusbar-safe-area` or even older versions of this plugin. You should make sure to uninstall those.
+
+### Ongoing Chromium bug
+
+There's a known Chromium bug that makes the webview report the bottom inset as more than `0px` even if the keyboard is visible. It will be fixed in Chromium version 144 ([source](https://issues.chromium.org/issues/457682720)).
+
+### Respecting the `viewport-fit=cover` tag
+
+When this plugin is installed, edge-to-edge mode is always enabled. Regardless of any other setting (except for broken webview versions of course). This means that the `viewport-fit=cover` tag is also not respected. So you should make sure any content loaded into the webview handles the `env(safe-area-inset-*)` accordingly. This is a known shortcoming of this plugin currently. I'm looking for ways to fix this, but haven't yet come up with a solution. For most use cases this shouldn't be a problem though. As most apps either do or don't support edge-to-edge. PRs to fix this are warmly welcomed!
+
+### Future work
+
+The Capacitor team themselves are also working on a [similar solution](https://github.com/ionic-team/capacitor/pull/8180). It's still a work in progress though. You should be aware that you might have to migrate once they've integrated that PR into their codebase.
--- android/src/main/java/com/getcapacitor/community/safearea/SafeAreaPlugin.java
+++ android/src/main/java/com/getcapacitor/community/safearea/SafeAreaPlugin.java
@@ -1,10 +1,192 @@
 package com.getcapacitor.community.safearea;
 
-import com.getcapacitor.JSObject;
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import androidx.annotation.Nullable;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.webkit.WebViewCompat;
+
 import com.getcapacitor.Plugin;
-import com.getcapacitor.PluginCall;
-import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @CapacitorPlugin(name = "SafeArea")
-public class SafeAreaPlugin extends Plugin {}
+public class SafeAreaPlugin extends Plugin {
+    private static final String viewportMetaJSFunction = """
+            function capacitorSafeAreaCheckMetaViewport() {
+                const meta = document.querySelectorAll("meta[name=viewport]");
+                if (meta.length == 0) {
+                    return false;
+                }
+                // get the last found meta viewport tag
+                const metaContent = meta[meta.length - 1].content;
+                return metaContent.includes("viewport-fit=cover");
+            }
+            
+            capacitorSafeAreaCheckMetaViewport();
+            """;
+
+    private int webViewMajorVersion;
+
+    // https://issues.chromium.org/issues/40699457
+    private static final int WEBVIEW_VERSION_WITH_SAFE_AREA_CORE_FIX = 140;
+
+    // https://issues.chromium.org/issues/457682720
+    private static final int WEBVIEW_VERSION_WITH_SAFE_AREA_KEYBOARD_FIX = 144;
+
+    private boolean hasMetaViewportCover = false;
+
+    @Override
+    public void load() {
+        super.load();
+
+        webViewMajorVersion = getWebViewMajorVersion();
+
+        this.bridge.getWebView().evaluateJavascript(viewportMetaJSFunction, (res) -> {
+            // @TODO: this doesn't work yet.
+            // Seems like a bug in Chromium that the safe area insets do not get updated upon changing the return inside `setOnApplyWindowInsetsListener`
+            // @TODO: ideally this should be rechecked in `onPageFinished` for example. Because some webpages might support edge-to-edge whereas others do not.
+            // hasMetaViewportCover = res.equals("true");
+
+            // Request new execution tree of `setOnApplyWindowInsetsListener`
+            this.bridge.getWebView().requestApplyInsets();
+        });
+
+        setupSafeAreaInsets();
+    }
+
+    private int getWebViewMajorVersion() {
+        PackageInfo packageInfo = WebViewCompat.getCurrentWebViewPackage(bridge.getContext());
+        Pattern pattern = Pattern.compile("(\\d+)");
+        Matcher matcher = pattern.matcher(packageInfo.versionName);
+
+        if (!matcher.find()) {
+            return 0;
+        }
+
+        String majorVersionStr = matcher.group(0);
+        int majorVersion = Integer.parseInt(majorVersionStr);
+
+        return majorVersion;
+    }
+
+    private void setupSafeAreaInsets() {
+        // View view = shouldPassthroughInsets ? getActivity().getWindow().getDecorView() : bridge.getWebView();
+        View view = getActivity().getWindow().getDecorView();
+
+        View parent = (ViewGroup) getBridge().getWebView().getParent();
+
+        ViewCompat.setOnApplyWindowInsetsListener(view, (v, windowInsets) -> {
+            boolean shouldPassthroughInsets = webViewMajorVersion >= WEBVIEW_VERSION_WITH_SAFE_AREA_CORE_FIX && hasMetaViewportCover;
+
+            if (shouldPassthroughInsets) {
+                // In case `shouldPassthroughInsets` was `false` before,
+                // we need to reset the view margins of `parent`
+                setViewMargin(parent, 0, 0, 0, 0);
+
+                // We need to correct for a possible shown IME
+                // NOTE: using `getBridge().getWebView()` instead of `parent` here, to prevent infinite loops
+                setViewMargin(getBridge().getWebView(), 0, 0, getBottomMargin(windowInsets, parent), 0);
+
+                return windowInsets;
+            }
+
+            // Optionally, we could allow users to workaround the older webview issues by passing through the insets as custom vars
+            // if (hasFixedWebView) {
+            //     Insets safeArea = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
+            //     injectSafeAreaCSS(safeArea.top, safeArea.right, safeArea.bottom, safeArea.left);
+            //     return windowInsets;
+            // }
+
+            Insets systemBarsInsets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
+            Insets imeInsets = windowInsets.getInsets(WindowInsetsCompat.Type.ime());
+            boolean keyboardVisible = windowInsets.isVisible(WindowInsetsCompat.Type.ime());
+
+            setViewMargin(parent, systemBarsInsets.top, systemBarsInsets.right, keyboardVisible ? imeInsets.bottom : systemBarsInsets.bottom, systemBarsInsets.left);
+
+            return WindowInsetsCompat.CONSUMED;
+        });
+    }
+
+    private void setViewMargin(View view, @Nullable Integer top, @Nullable Integer right, @Nullable Integer bottom, @Nullable Integer left) {
+        ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+        if (top != null) {
+            mlp.topMargin = top;
+        }
+        if (right != null) {
+            mlp.rightMargin = right;
+        }
+        if (bottom != null) {
+            mlp.bottomMargin = bottom;
+        }
+        if (left != null) {
+            mlp.leftMargin = left;
+        }
+        view.setLayoutParams(mlp);
+    }
+
+    private int getBottomMargin(WindowInsetsCompat windowInsets, View parent) {
+        Insets systemBarsInsets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
+        Insets imeInsets = windowInsets.getInsets(WindowInsetsCompat.Type.ime());
+        boolean keyboardVisible = windowInsets.isVisible(WindowInsetsCompat.Type.ime());
+
+        if (!keyboardVisible) {
+            return 0;
+        }
+
+        // Shrink the view so that it's shown in the area available right above the IME
+        int bottomMargin = imeInsets.bottom;
+
+        if (systemBarsInsets.bottom > 0 && webViewMajorVersion < WEBVIEW_VERSION_WITH_SAFE_AREA_KEYBOARD_FIX) {
+            // https://issues.chromium.org/issues/457682720
+            // this is a workaround to push the webview behind the keyboard for webview versions that have a bug
+            // that causes the bottom inset to be incorrect if the IME is visible
+            bottomMargin = imeInsets.bottom - systemBarsInsets.bottom;
+        }
+
+        if (bottomMargin > parent.getHeight()) {
+            // This is needed to workaround a bug that when an IME is visible before app start,
+            // setting the MarginLayoutParams causes issues.
+            bottomMargin = parent.getHeight();
+        }
+
+        if (bottomMargin < 0) {
+            bottomMargin = 0;
+        }
+
+        return bottomMargin;
+    }
+
+    public enum SystemBarsStyle {
+        LIGHT,
+        DARK
+    }
+
+
+    public static void setSystemBarsStyle(Activity activity, SystemBarsStyle style) {
+        // @TODO: calling this method from within the plugin context unfortunately doesn't work for some reason. For now we need to do that in the `MainActivity` instead.
+        Window window = activity.getWindow();
+        WindowInsetsControllerCompat windowInsetsControllerCompat = WindowCompat.getInsetsController(window, window.getDecorView());
+        if (style == SystemBarsStyle.DARK) {
+            windowInsetsControllerCompat.setAppearanceLightNavigationBars(false);
+            windowInsetsControllerCompat.setAppearanceLightStatusBars(false);
+            window.getDecorView().setBackgroundColor(Color.BLACK);
+        } else {
+            windowInsetsControllerCompat.setAppearanceLightNavigationBars(true);
+            windowInsetsControllerCompat.setAppearanceLightStatusBars(true);
+            window.getDecorView().setBackgroundColor(Color.WHITE);
+        }
+    }
+}
+
